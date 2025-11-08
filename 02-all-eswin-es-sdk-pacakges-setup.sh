f0cb0ece148dd3916d9857a6b66d4f63ac429889
Deepen Dhulla <deepen@deependhulla.com>
2025-11-08 16:31:17 +0530

    update on Sat Nov  8 04:31:17 PM IST 2025

--- 02-all-eswin-es-sdk-pacakges-setup.sh
+++ 02-all-eswin-es-sdk-pacakges-setup.sh
@@ -6,4 +6,4 @@ apt-get -y install es-sdk-*
 ## on a Linux intel or AMD x86 ..for SDK development 
 ## suggested to download all files from 
 ## https://fast-mirror.isrc.ac.cn/rockos/nn_tools/EIC7x_Release_20250130/
-
+## special working on computer vision model  -- guide -- https://milkv.io/docs/megrez/development-guide/ENNP-SDK/yolov3
