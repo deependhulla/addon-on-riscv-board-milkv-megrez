38dbf642ac968cd8b18f5c0e13d0137e87d261b8
Deepen Dhulla <deepen@deependhulla.com>
2025-11-08 12:55:25 +0530

    update on Sat Nov  8 12:55:25 PM IST 2025

---
+++ 04-only-eswin-es-sdk-for-npu-use-major.sh
@@ -0,0 +1,9 @@
+#/!bin/bash
+## install major packages of milkv megrez eswin es-sdk useful for various npu testing samd sample
+
+apt-get -y install es-sdk-*
+
+## on a Linux intel or AMD x86 ..for SDK development 
+## suggested to download all files from 
+## https://fast-mirror.isrc.ac.cn/rockos/nn_tools/EIC7x_Release_20250130/
+
