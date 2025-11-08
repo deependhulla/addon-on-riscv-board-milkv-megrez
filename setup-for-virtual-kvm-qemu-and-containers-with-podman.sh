d793e74c1726e5d7299dc7c35d1365dbe66080d3
Deepen Dhulla <deepen@deependhulla.com>
2025-11-08 19:15:28 +0530

    update on Sat Nov  8 07:15:28 PM IST 2025

--- setup-for-virtual-kvm-qemu-and-containers-with-podman.sh
+++ setup-for-virtual-kvm-qemu-and-containers-with-podman.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-apt -y install firmware-linux-nonfree firmware-realtek
+## let get LXC, VM, Podman -Contianer up on this PC.
+
+## if you get toomany firmare error and want to install
+##apt -y install firmware-linux-nonfree firmware-realtek
 
 ##https://milkv.io/docs/megrez/development-guide/kvm
 ## its not active default
 modprobe kvm
 
-apt -y  install cockpit bridge-utils cockpit-machines cockpit-podman realmd sscg u-boot-qemu qemu-efi-riscv64 
+apt -y  install cockpit bridge-utils cockpit-machines cockpit-podman realmd sscg u-boot-qemu qemu-efi-riscv64 virt-manager lxc 
 
 
 ## use virt manager to create VM of RISC-V Linux with u-boot guide given in milkv website.
