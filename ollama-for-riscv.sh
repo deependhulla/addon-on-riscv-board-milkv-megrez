6d21d49889a9a0c58e7f843672ce31fa5b8aa302
Deepen Dhulla <deepen@deependhulla.com>
2025-11-08 21:24:53 +0530

    update on Sat Nov  8 09:24:53 PM IST 2025

--- ollama-for-riscv.sh
+++ ollama-for-riscv.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 cd /opt/
-git clone https://github.com/deependhulla/ollama-for-riscv
+git clone --recurse-submodules https://github.com/deependhulla/ollama-for-riscv
+
+cd /opt/ollama-for-riscv
+go build .
+ln -s /opt/ollama-for-riscv/ollama /usr/local/bin/ollama
+
+/bin/cp /opt/ollama-for-riscv/ollama.service /etc/systemd/system/
+systemctl daemon-reload
+
+systemctl start ollama
+## download smallest model
+ollama pull smollm:135m
+ollama pull granite4:350m
+
+## Test it
+ollama run smollm:135m
+
+#ollama run granite4:350m
 
 ### orginal thanks -credit to https://github.com/mengzhuo/ollama
-
+## https://www.jeffgeerling.com/blog/2025/how-build-ollama-run-llms-on-risc-v-linux
