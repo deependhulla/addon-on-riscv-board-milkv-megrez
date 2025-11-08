40eadfa30512d45746e04040f3b7d65fd2b30745
Deepen Dhulla <deepen@deependhulla.com>
2025-11-08 19:09:59 +0530

    update on Sat Nov  8 07:09:59 PM IST 2025

--- setup-for-virtual-kvm-qemu-and-containers-with-podman.sh
+++ setup-for-virtual-kvm-qemu-and-containers-with-podman.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-apt -y firmware-linux-nonfree firmware-realtek
+apt -y install firmware-linux-nonfree firmware-realtek
 
-apt -y  install cockpit bridge-utils cockpit-machines cockpit-podman 
+##https://milkv.io/docs/megrez/development-guide/kvm
+## its not active default
+modprobe kvm
+
+apt -y  install cockpit bridge-utils cockpit-machines cockpit-podman realmd sscg u-boot-qemu qemu-efi-riscv64 
+
+
+## use virt manager to create VM of RISC-V Linux with u-boot guide given in milkv website.
+#latter you can manage it via cockpit VM
+## while podman container are easier to manage
 
