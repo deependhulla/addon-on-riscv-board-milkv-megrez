ff7c7eb3a439a7643d6813e70b06beaf35bb1243
Deepen Dhulla <deepen@deependhulla.com>
2025-11-09 12:07:10 +0530

    update on Sun Nov  9 12:07:10 PM IST 2025

--- frankenphp-build.sh
+++ frankenphp-build.sh
@@ -61,18 +61,7 @@ xcaddy build \
     --with github.com/dunglas/mercure/caddy \
     --with github.com/dunglas/vulcain/caddy \
 
-
-
-
-
-CGO_ENABLED=1 \
-XCADDY_GO_BUILD_FLAGS="-ldflags='-w -s' -tags=nobadger,nomysql,nopgx,nobrotli,nowatcher" \
-CGO_CFLAGS=$(php-config --includes) \
-CGO_LDFLAGS="$(php-config --ldflags) $(php-config --libs)" \
-xcaddy build \
-    --output frankenphp \
-    --with github.com/dunglas/frankenphp/caddy \
-    --with github.com/dunglas/mercure/caddy \
-    --with github.com/dunglas/vulcain/caddy \
+echo "<?php phpinfo(); ?>" >  /opt/frankenphp/index.php
+/opt/frankenphp/frankenphp php-server 
 
 
