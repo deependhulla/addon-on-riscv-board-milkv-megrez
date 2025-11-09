82b4eb19eb3f2a655303fc85bf0d0bba63eeba37
Deepen Dhulla <deepen@deependhulla.com>
2025-11-09 11:21:59 +0530

    update on Sun Nov  9 11:21:59 AM IST 2025

--- extra-files/php-build.sh
+++ extra-files/php-build.sh
@@ -3,7 +3,7 @@
 #To Complie PHP first ,then get frankenphp
 #https://frankenphp.dev/docs/compile/
 
-apt-get install re2c libtool pkg-config automake autoconf libxml2-dev libjpeg-dev libpng-dev libwebp-dev libcurl4-openssl-dev libssl-dev libmcrypt-dev mcrypt libmysqlclient-dev libsqlite3-dev libpq-dev autopoint debhelper dh-autoreconf dh-strip-nondeterminism dwz gettext intltool-debian libarchive-cpio-perl libarchive-zip-perl libdebhelper-perl libfile-stripnondeterminism-perl libmail-sendmail-perl libpcre2-32-0 libpcre2-dev libpcre2-posix3 libsys-hostname-long-perl shtool brotli  libbrotli-dev libonig-dev libbison-dev bison brotli-rs  libbrotli-dev golang-github-andybalholm-brotli-dev libbrotli1 libbz2-dev libgmp-dev libzip-dev libzip-dev libwebp-dev libjpeg-dev libpng-dev libfreetype-dev libicu-dev libgettextpo-dev libldap2-dev libpq-dev libtidy-dev libxslt-dev libsnmp-dev libsodium-dev gettext
+apt-get install re2c libtool pkg-config automake autoconf libxml2-dev libjpeg-dev libpng-dev libwebp-dev libcurl4-openssl-dev libssl-dev libmcrypt-dev mcrypt libmysqlclient-dev libsqlite3-dev libpq-dev autopoint debhelper dh-autoreconf dh-strip-nondeterminism dwz gettext intltool-debian libarchive-cpio-perl libarchive-zip-perl libdebhelper-perl libfile-stripnondeterminism-perl libmail-sendmail-perl libpcre2-32-0 libpcre2-dev libpcre2-posix3 libsys-hostname-long-perl shtool brotli  libbrotli-dev libonig-dev libbison-dev bison brotli-rs  libbrotli-dev golang-github-andybalholm-brotli-dev libbrotli1 libbz2-dev libgmp-dev libzip-dev libzip-dev libwebp-dev libjpeg-dev libpng-dev libfreetype-dev libicu-dev libgettextpo-dev libldap2-dev libpq-dev libtidy-dev libxslt-dev libsnmp-dev libsodium-dev gettext libffi-dev
 
 
 cd /opt/
@@ -34,9 +34,13 @@ cd /opt/php-8.4.14
  --with-config-file-path=/etc --with-zlib --with-openssl --with-curl --with-pdo-mysql=mysqlnd \
  --enable-mbstring --enable-soap --enable-sockets --enable-bcmath --with-bz2 \
  --enable-calendar --enable-exif --enable-ftp --with-gmp --enable-shmop \
- --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-mysqli \
- --enable-pcntl --with-zip --with-gd --enable-intl --with-gettext --with-ldap \
+ --enable-sysvmsg --enable-sysvsem --enable-sysvshm  \
+ --enable-pcntl --with-zip --enable-intl --with-gettext --with-ldap \
  --with-pdo-pgsql --with-pgsql --with-tidy --with-xsl --with-snmp --with-sodium --with-ffi
+
+make -j"$(getconf _NPROCESSORS_ONLN)"
+make test
+make install
 
 cd /opt/
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
